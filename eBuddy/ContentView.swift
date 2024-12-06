60ab52c573dca7ecd8b1b4a9906accb54136b4eb
Felix Liman <felixlim75@gmail.com>
2024-12-07 01:19:13 +0700

    Initial Commit

---
+++ eBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eBuddy
+//
+//  Created by Felix Liman on 07/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eBuddy/eBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  eBuddyApp.swift
+//  eBuddy
+//
+//  Created by Felix Liman on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct eBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
